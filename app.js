d83b90df9b6926c2b49d5ca097e7da930c691563
Ryan Keller <rjkeller_01@hotmail.com>
2021-02-19 21:57:23 -0700

    added a couple of classes

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+
+
+let cursors = {
+    currentValue: 0,
+    cost: 15,
+
+}
+
+let grandmas = {
+    currentValue: 0,
+    cost: 150,
+}
+
+let generators = {
+    cursors,
+    grandmas
+}
+
 var brightIdeas = 0;
 var clickingPower= 10;
-var cursorCost = 15;
-var cursors = 0;
 
-var grandmaCost = 150;
-var grandmas = 0;
+
 function buyCursor() {
+    let cursorCost = cursors.cost *1.15 ** cursors.currentValue;
     if (brightIdeas >= cursorCost){
             brightIdeas -= cursorCost;
-            cursors += 1;
-            cursorCost = Math.round(cursorCost * 1.15);
+            cursors.currentValue += 1;
             reRender();
 
 
@@ -17,20 +32,21 @@ function buyCursor() {
 }
 
 function buyGrandma() {
+    let grandmaCost = grandmas.cost * 1.25 ** grandmas.currentValue;
     if (brightIdeas >= grandmaCost){
             brightIdeas -= grandmaCost;
-            grandmas += 1;
-            grandmaCost = Math.round(grandmaCost * 1.25);
+            grandmas.currentValue += 1;
             reRender();
 
     }
 }
 function reRender() {
     document.getElementById('brightIdeas').innerHTML = brightIdeas;
-    document.getElementById('grandmaCost').innerHTML = grandmaCost;
-    document.getElementById('grandmas').innerHTML = grandmas;
-    document.getElementById('cursorCost').innerHTML = cursorCost;
-    document.getElementById('cursors').innerHTML = cursors;
+    document.getElementById('grandmaCost').innerHTML = grandmas.cost * 1.25 ** grandmas.currentValue;;
+    document.getElementById('grandmas').innerHTML = grandmas.currentValue;
+    document.getElementById('cursorCost').innerHTML = cursors.cost *1.15 ** cursors.currentValue;
+
+    document.getElementById('cursors').innerHTML = cursors.currentValue;
     document.title = brightIdeas + "Ideas - the main stream"
     updateIdeasPerSecond();
 }
@@ -42,12 +58,12 @@ function addToBrightIdeas(amount) {
 }
 
 function updateIdeasPerSecond(){
-    brightIdeasPerSecond = cursors + grandmas * 25;
+    brightIdeasPerSecond = cursors.currentValue + grandmas.currentValue * 25;
     document.getElementById("brightIdeasPerSecond").innerHTML = brightIdeasPerSecond;
 }
 
 setInterval(function() {
-    brightIdeas += cursors;
-    brightIdeas += grandmas * 25;
+    brightIdeas += cursors.currentValue;
+    brightIdeas += grandmas.currentValue * 25;
     reRender()
 }, 1000);
